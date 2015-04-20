81d5b63861753306f2b11dcaf564ab5b38643c00
Haruka Ono <HarukaOno@Haruka-no-MacBook-Pro.local>
2015-04-20 20:55:34 +0900

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Haruka Ono on 2015/04/20.
+//  Copyright (c) 2015年 maximum80. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
