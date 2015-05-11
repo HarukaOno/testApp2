438e6f00b2897b0f0d2802e9dbc44181836ef492
yukako <yukako@tsudaumeko-no-MacBook-Pro-2.local>
2015-05-11 12:43:36 +0900

    ラッキーアイテムの追加

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     let divineButton: UIButton = UIButton()
     
     let resultLabel: UILabel = UILabel()
+    
+    let LuckyLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
+    
+    let charmLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
 
     let status: [(weight:Int, fortune:String, color:UIColor)] =
         [(0,"大吉", UIColor.redColor()), (3,"中吉", UIColor.yellowColor()),
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
          (17,"凶", UIColor.grayColor()), (18,"大凶", UIColor.blueColor())
         ]
     // weightは重みをつけるための値
+    
+    let LuckyCharm : [String] = ["ピンクの小物","テニスラケット","ぬいぐるみ","青の財布","赤い靴","黒い傘","緑の髪留め","指輪","トライアングル","リップクリーム","黄色いメモ帳","漫画","ゲーム機","パワーストーン","招き猫","ミサンガ","四葉のクローバー","天使"]
 
 
     override func viewDidLoad() {
@@ -47,11 +53,23 @@ class ViewController: UIViewController {
         divineButton.backgroundColor = UIColor.redColor()
         divineButton.layer.masksToBounds = true
         
+        LuckyLabel.text = "ラッキーアイテム"
+        LuckyLabel.textAlignment = NSTextAlignment.Center
+        LuckyLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: 380)
+        self.view.addSubview(LuckyLabel)
+
+
+        charmLabel.text = "????"
+        charmLabel.textAlignment = NSTextAlignment.Center
+        charmLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: 400)
+        self.view.addSubview(charmLabel)
+        
+        
         divineButton.setTitle("運勢を占う", forState: UIControlState.Normal)
         divineButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
         
         divineButton.layer.cornerRadius = 10.0
-        divineButton.layer.position = CGPoint(x: self.view.frame.width / 2,y : 400)
+        divineButton.layer.position = CGPoint(x: self.view.frame.width / 2,y : 450)
         divineButton.tag = 1
         divineButton.addTarget(self, action: "onClickDivineButton:", forControlEvents: .TouchUpInside)
         self.view.addSubview(divineButton)
@@ -72,6 +90,7 @@ class ViewController: UIViewController {
         }
         resultLabel.textColor = status[i].color
         resultLabel.text = status[i].fortune + String(random)
+        charmLabel.text = LuckyCharm[i]
     }
 }
 
